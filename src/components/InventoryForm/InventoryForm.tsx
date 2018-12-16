92d5060a3362dc66d611d76517c4e7d5e93baccb
alpolex <eddine.zeroual@gmail.com>
2018-12-16 16:26:16 +0100

    add furniture with his configuration, to a list of furnitures

--- src/components/InventoryForm/InventoryForm.tsx
+++ src/components/InventoryForm/InventoryForm.tsx
@@ -1,7 +1,6 @@
 import React, { EventHandler, useEffect } from "react";
 import TextInput from "../TextInput";
 import volumeCalculator from "../../utils/volumeCalculator";
-import Button from "../Button";
 
 const msg = Object.freeze({
   furnitureLabel: "Nom du meuble",
@@ -11,17 +10,27 @@ const msg = Object.freeze({
   addFurniture: "Ajouter"
 });
 
+type Furniture = {
+  furnitureVolume: number;
+  furnitureHeight: number;
+  furnitureWidth: number;
+  furnitureLength: number;
+  furnitureLabel: string;
+};
+
 interface Props extends React.FormHTMLAttributes<HTMLFormElement> {}
 
 const InventoryForm = () => {
+  const [furnitureLabel, setFurnitureLabel] = React.useState('');
   const [furnitureVolume, setFurnitureVolume] = React.useState(0);
   const [furnitureWidth, setFurnitureWidth] = React.useState(0);
   const [furnitureHeight, setFurnitureHeight] = React.useState(0);
   const [furnitureLength, setFurnitureLength] = React.useState(0);
+  const [furnitureList, setFurnitureList] = React.useState<ReadonlyArray<Furniture>>([]);
 
+  useEffect(() => console.log(furnitureList), [furnitureList])
   useEffect(
     () => {
-      console.log("coucou");
       setFurnitureVolume(
         volumeCalculator(furnitureWidth, furnitureHeight, furnitureLength)
       );
@@ -29,30 +38,34 @@ const InventoryForm = () => {
     [furnitureWidth, furnitureHeight, furnitureLength]
   );
 
-  const handleWidthValueChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setFurnitureWidth(parseFloat(event.currentTarget.value));
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    setFurnitureList([{
+      furnitureVolume: furnitureVolume,
+      furnitureHeight: furnitureHeight,
+      furnitureWidth: furnitureWidth,
+      furnitureLength: furnitureLength,
+      furnitureLabel: furnitureLabel
+    }, ...furnitureList])
   };
 
-  const handleHeightValueChange = (
-    event: React.FormEvent<HTMLInputElement>
-  ) => {
-    setFurnitureHeight(parseFloat(event.currentTarget.value));
-  };
+  const handleLabelValueChange = (event: React.FormEvent<HTMLInputElement>) => setFurnitureLabel(event.currentTarget.value);
+ 
+  const handleWidthValueChange = (event: React.FormEvent<HTMLInputElement>) => setFurnitureWidth(parseFloat(event.currentTarget.value));
+  
+  const handleHeightValueChange = (event: React.FormEvent<HTMLInputElement>) => setFurnitureHeight(parseFloat(event.currentTarget.value));
 
-  const handleLengthValueChange = (
-    event: React.FormEvent<HTMLInputElement>
-  ) => {
-    setFurnitureLength(parseFloat(event.currentTarget.value));
-  };
+  const handleLengthValueChange = (event: React.FormEvent<HTMLInputElement>) => setFurnitureLength(parseFloat(event.currentTarget.value));
 
   return (
     <>
-      <form>
-        <TextInput name={msg.furnitureLabel} />
-        <TextInput onChange={handleLengthValueChange} name={msg.lengthLabel} />
-        <TextInput onChange={handleWidthValueChange} name={msg.widthLabel} />
-        <TextInput onChange={handleHeightValueChange} name={msg.heightLabel} />
-        <Button>{msg.addFurniture}</Button>
+      <form onSubmit={handleSubmit}>
+        <TextInput onChange={handleLabelValueChange} name={msg.furnitureLabel} required/>
+        <TextInput onChange={handleLengthValueChange} name={msg.lengthLabel} required/>
+        <TextInput onChange={handleWidthValueChange} name={msg.widthLabel} required/>
+        <TextInput onChange={handleHeightValueChange} name={msg.heightLabel} required/>
+        <input type='submit' value={msg.addFurniture} />
       </form>
       <span>{furnitureVolume}</span>
     </>
--- src/components/InventoryForm/__tests__/InventoryForm.spec.tsx
+++ src/components/InventoryForm/__tests__/InventoryForm.spec.tsx
@@ -1,11 +1,13 @@
-import { render } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import React from 'react';
 import InventoryForm from '../InventoryForm';
+
+afterEach(cleanup)
 
 describe('InventoryForm', () => {
     it('should show inputs', () => {
         const { container } = render(<InventoryForm />)
-
         expect(container.querySelectorAll('form')).toHaveLength(1);
+        expect(container.querySelectorAll('input')).toHaveLength(5);
     })
 })
